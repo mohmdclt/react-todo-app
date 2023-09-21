1b939f0911e91e0b99f00aab0d4c492b61a387c6
Muhammed K K <m.calicut11@gmail.com>
2023-09-21 17:49:43 +0530

    Todo list displayed with checkbox Status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css'
 import {useState} from "react"
-
 function App() {
   const [Todos, setTodos] = useState([]);
   const [Todo, setTodo] = useState('');
-  
-  return (
+   return (
     <div className="app">
       <div className="mainHeading">
         <h1>ToDo List</h1>
@@ -16,22 +14,38 @@ function App() {
       </div>
       <div  className="input">
         <input value={Todo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setTodos([...Todos,Todo])} className="fas fa-plus"></i>
-
-        {console.log(Todo,Todos)}
+        <i onClick={()=>setTodos([...Todos,{id:Date.now(),text:Todo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         { Todos.map((obj)=>{
 
        return( <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>{obj}</p>
+            <input onChange={(e)=>{
+              
+              console.log(e.target.checked)
+              console.log(obj)
+            setTodos(Todos.filter(obj2=>{
+              if(obj2.id===obj.id) {
+               obj2.status=e.target.checked
+              }
+              return obj2
+            }))
+            }}
+            value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
         </div>)})}
+
+        { Todos.map((obj)=>{
+          if (obj.status) {
+          return(<h1>{obj.text}</h1>)
+        }
+        return null
+      })}
       </div>
     </div>
   );
