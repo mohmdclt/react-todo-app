42e062107939f000bbee754ca6afd6178b4f2f4e
Muhammed K K <m.calicut11@gmail.com>
2023-09-21 23:56:33 +0530

    Deletion operation succesfuly Added

--- src/App.js
+++ src/App.js
@@ -37,7 +37,14 @@ function App() {
             <p>{obj.text}</p>
           </div>
           <div className="right">
-            <i className="fas fa-times"></i>
+            <i onClick={()=>
+            setTodos(Todos.filter(obj3=>{
+              if(obj3.id!==obj.id)
+              return obj3
+            }
+              
+             ))
+             } className="fas fa-times"></i>
           </div>
         </div>)})}
 
